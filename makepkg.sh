255c080d9aaeee7e44a706fe24fec6d9f4f6da2c
Kyle Smith <kyleafmine@gmail.com>
2022-08-10 23:39:23 -0300

    i am at my wits end

--- makepkg.sh
+++ makepkg.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
 # this runs in an arch container
-makepkg . -d
+useradd -m build && echo "build ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/build
+chmod -R 0777 ../*
+chown -R build ../*
+su -u build /usr/bin/bash -c "makepkg . -d"
