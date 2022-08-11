f35caa60d2c8cbca93c7cfb65fefcc7eaed5b2b0
Kyle Smith <81243802+EggAllocationService@users.noreply.github.com>
2022-08-11 00:42:04 -0300

    revert chown to get git to be quiet about security issues

--- makepkg.sh
+++ makepkg.sh
@@ -4,3 +4,5 @@ useradd -m build && echo "build ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/build
 chmod -R 0777 ../*
 chown -R build ../*
 su build -s /usr/bin/bash -c "makepkg . -d"
+chown -R root ../*
+chmod -R 744 ../*
