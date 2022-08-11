42f0ab54b9e85f50ae7e6975ede8919f518c4982
Kyle Smith <81243802+EggAllocationService@users.noreply.github.com>
2022-08-11 01:07:40 -0300

    Less extreme makepkg

--- makepkg.sh
+++ makepkg.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/bash
 # this runs in an arch container
 useradd -m build && echo "build ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/build
-chmod -R 0777 ../*
-chown -R build ../*
+chown -R build ../workspace
 su build -s /usr/bin/bash -c "makepkg . -d"
-chown -R root ../*
-chmod -R 744 ../*
+chown -R root ../workspace
+
