829839caf7e6ef9a3c72c2607e8d0abb1082fdbe
Kyle Smith <81243802+EggAllocationService@users.noreply.github.com>
2022-08-11 00:46:42 -0300

    Add our directory just to be safe

--- release.sh
+++ release.sh
@@ -24,6 +24,7 @@ REPOSITORY="${REPO_NAME:-"vscodium"}"
 
 # git workaround
 git config --global --add safe.directory "*"
+git config --global --add safe.directory '/__w/vscodium-transparency/vscodium-transparency'
 
 
 for FILE in *
