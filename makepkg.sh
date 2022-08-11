47595e199d250c91050e3f06158e72113f2fd11a
Kyle Smith <81243802+EggAllocationService@users.noreply.github.com>
2022-08-10 23:52:06 -0300

    Update makepkg.sh

--- makepkg.sh
+++ makepkg.sh
@@ -3,4 +3,4 @@
 useradd -m build && echo "build ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/build
 chmod -R 0777 ../*
 chown -R build ../*
-su -u build /usr/bin/bash -c "makepkg . -d"
+su build -s /usr/bin/bash -c "makepkg . -d"
